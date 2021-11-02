1c3cd217d0cfec2f4bc9c63fd90addcea447ce39
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-11-02 23:46:52 +0900

    csvファイルの作成、2ページ目以降のデータを取得するコードを追加

--- kaiten_heiten_scraping/takoyaki.py
+++ kaiten_heiten_scraping/takoyaki.py
@@ -4,35 +4,19 @@ from urllib import request
 import datetime
 import numpy as np
 
-def period(year,month,year_s = 2019,year_e = 2019,month_s = 3,month_e = 4):
-    res = False
-    if (year<=year_e) & (year >=year_s) & (month>=month_s) & (month<=month_e):
-        res = 2
-    if year >= year_e:
-        if year > year_e:
-            res = 1
-        else:
-            if month>month_e:
-                res = 1
-
-    return res
 
 
-
-def main():
+def scrape():
     dic = []
-    states = [0]
-    for state in range(len(states)):
-        url = 'https://kaiten-heiten.com/category/restaurant/takoyaki/?s=%E3%80%90%E9%96%8B%E5%BA%97%E3%80%91'
+    for i in range(1,22):
+        # url = 'https://kaiten-heiten.com/category/restaurant/takoyaki/page/'+str(i)+'/?s=%E3%80%90%E9%96%8B%E5%BA%97%E3%80%91'
+        url = 'https://kaiten-heiten.com/category/restaurant/takoyaki/page/'+str(i)+'/?s=%E3%80%90%E9%96%8B%E5%BA%97%E3%80%91'
         response = request.urlopen(url)
         soup = BeautifulSoup(response,'html.parser')
-
         for a in soup.find_all('a', class_="post_links"): 
             link = a.get('href')
             date = a.text[6:16]
             all_data = a.text
-            # print(all_data)
-            # print(date)
             s_flag = False
             shop_name = ""
             for i in range(len(all_data)):
@@ -60,6 +44,9 @@ def main():
             address = address_data.text
             index = 0
             while True:
+                if index>=len(address):
+                    print(address)
+                    break
                 prefecture+=address[index]
                 if address[index]=='都' or address[index]=='道' or address[index]=='府' or address[index]=='県':
                     break
@@ -74,4 +61,9 @@ def main():
 
 
 
-print(main())
+if __name__ == "__main__":
+    data = scrape()
+    f = open('takoyaki.csv', 'w')
+    for d in data:
+        f.write(d[0]+","+d[1]+","+d[2]+","+d[3]+"\n")
+    f.close()
