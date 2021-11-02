5aea9c60c98502eb1c007bc75ed6d889dcb7b8e9
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-11-02 21:07:26 +0900

    たこ焼き店の1ページ目の開店年月、都道府県、店名、電話番号を返す関数の実装

--- kaiten_heiten_scraping/takoyaki.py
+++ kaiten_heiten_scraping/takoyaki.py
@@ -19,8 +19,8 @@ def period(year,month,year_s = 2019,year_e = 2019,month_s = 3,month_e = 4):
 
 
 
-def main(year_s = 2020,year_e = 2020,month_s = 4,month_e = 4):
-    dic = {}
+def main():
+    dic = []
     states = [0]
     for state in range(len(states)):
         url = 'https://kaiten-heiten.com/category/restaurant/takoyaki/?s=%E3%80%90%E9%96%8B%E5%BA%97%E3%80%91'
@@ -30,20 +30,21 @@ def main(year_s = 2020,year_e = 2020,month_s = 4,month_e = 4):
         for a in soup.find_all('a', class_="post_links"): 
             link = a.get('href')
             date = a.text[6:16]
-            all_data = a.text 
-            print(date)
+            all_data = a.text
             # print(all_data)
-            flag = False
+            # print(date)
+            s_flag = False
             shop_name = ""
             for i in range(len(all_data)):
-                if flag:
+                #店の名前に関する処理
+                if s_flag:
                     if all_data[i]=='\n':
-                        flag=False
+                        s_flag=False
                     else:
                         shop_name+=all_data[i]
                 elif all_data[i]=='】':
-                    flag = True
-            print(shop_name)
+                    s_flag = True
+            # print(shop_name)
 
 
             # if dic.get(shop_name) is None:
@@ -52,80 +53,25 @@ def main(year_s = 2020,year_e = 2020,month_s = 4,month_e = 4):
 
 
             url = link
-#             response = request.urlopen(url)
-#             soup = BeautifulSoup(response,'html.parser')
-
-#             shop_list = soup.find_all('span', class_='post_time')
-
-#             year_last = int(shop_list[-1].text[:5])
-#             month_last = int(shop_list[-1].text[6:8])
-
-#             for a in shop_list:      
-
-#                 year = int(a.text[:5])
-#                 month = int(a.text[6:8])
-
-
-#                 if period(year, month,year_s,year_e,month_s,month_e) == 2:
-#                     dic[region][state] += 1
-
-#                 cout = 0
-#                 flag = 0
-
-#                 while period(year_last, month_last,year_s,year_e,month_s,month_e)>=1:
-
-#                     next_p = soup.find('a',class_='next page-numbers')
-
-#                     if soup.find_all('a',class_='next page-numbers') is not None:
-#                         link = next_p.get('href')
-#                     else:
-#                         break
-
-#                     url = link
-#                     response = request.urlopen(url)
-#                     soup = BeautifulSoup(response,'html.parser')
+            response = request.urlopen(url)
+            soup = BeautifulSoup(response,'html.parser')
+            prefecture = ""
+            address_data = soup.find('h3')
+            address = address_data.text
+            index = 0
+            while True:
+                prefecture+=address[index]
+                if address[index]=='都' or address[index]=='道' or address[index]=='府' or address[index]=='県':
+                    break
+                else:
+                    index+=1
+            phone = soup.select('td')[3].text
+            dic.append([date,prefecture,shop_name,phone])
+    return dic
 
 
-#                     shop_list = soup.find_all('span', class_='post_time')
-
-#                     year_last = int(shop_list[-1].text[:5])
-#                     month_last = int(shop_list[-1].text[6:8])
 
 
-#                     # print(year_last,month_last)
-
-#                     for a in shop_list:        
-
-#                         year = int(a.text[:5])
-#                         month = int(a.text[6:8])
 
 
-#                         if period(year, month):
-#                             dic[region][state] += 1
-
-
-#     regions = list(dic.keys())
-#     vals_ = np.array(list(dic.values()))
-
-#     hk = int(22)
-
-#     hk_name = ['北海道']
-#     hk_vals = np.array([vals_[:hk,0].sum(), vals_[:hk,1].sum()])
-
-#     regions = hk_name + regions[hk:]
-#     vals = [list(hk_vals)] + list(vals_[hk:])
-
-
-#     # ratio == N_o - N_c / N
-
-#     ratio = np.zeros(len(regions))
-
-#     for i in range(len(regions)):
-#         ratio[i] = (vals[i][1] - vals[i][0]) / (vals[i][1] + vals[i][0])
-
-#     return regions, vals, ratio, vals_
-
-
-# regions_2020, vals_2020, ratio_2020, vals_2020 = main(2020,2020,3,4)
-# regions_2019, vals_2019, ratio_2019, vals_2019 = main(2019,2019,3,4)
-main()
+print(main())
