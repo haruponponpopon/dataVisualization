b06b54eda30e8720dce2ce70b66b399b5c3a4e76
Shiho Noda <shihonoda@ShihonoMacBook-Air.local>
2021-11-11 02:19:14 +0900

    layer graphのメモリを日付に(要検討) & 選択したものだけを表示に変更

--- tabelog_scraping/csv_scraping.py
+++ tabelog_scraping/csv_scraping.py
@@ -65,7 +65,7 @@ def do_scraping(shop_name):
         genres.append(genre.getText())
     return genres
 
-input_filename = "./unagi_out.csv"
+input_filename = "./data/cafe_out.csv"
 df = pd.read_csv(input_filename)
 df.set_index("ShopName", inplace = True)
 ###########二回目以降のファイルのときはコメントアウト!!!##################### 
@@ -73,6 +73,7 @@ df.set_index("ShopName", inplace = True)
 ###########################################################
 for shop_name in df.index.values:
     if isinstance(df.loc[shop_name, "genres"], pd.core.series.Series):
+        
         print("error! There is duplication: ", shop_name)
         genre = do_scraping(shop_name)
         add_genre  = ""
@@ -98,4 +99,4 @@ for shop_name in df.index.values:
             add_genre = add_genre + " #" + g
     print(shop_name , " : " , add_genre)
     df.loc[shop_name, "genres"] = add_genre
-    df.to_csv("./unagi_out.csv")
+    df.to_csv("./data/cafe_out.csv")
