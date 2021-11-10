e430bbe8e2fb7ad1b8248fbfb7194890c421e3e1
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-11-10 14:36:28 +0900

    うなぎ店のデータ集計完了。適切なフォーマットでのjsファイルの作成

--- data_analysis/layer_graph_data.py
+++ data_analysis/layer_graph_data.py
@@ -1,37 +1,33 @@
 #データの読み込み、分析
-f_r = open('sampledata.csv', 'r')
+f_r = open('unagi.csv', 'r')
 
 datalist = f_r.readlines()
 mydict = {}
 genres_dict = {}
+year_index = 1
 for i in range(1,len(datalist)):
-  arr = datalist[i].split(',')
-  genres = arr[5].split('#')
-  for j in (1,len(genres)-1):
-      if j==len(genres)-1:
-          result = mydict.get((genres[j][0:len(genres[j])-1], arr[0][0:7]))
-          genres_dict[genres[j][0:len(genres[j])-1]]=1
-          if result==None:
-              mydict[(genres[j][0:len(genres[j])-1], arr[0][0:7])] = 1
-          else:
-              mydict[(genres[j][0:len(genres[j])-1], arr[0][0:7])] = result + 1
-      else:
-          genres_dict[genres[j][0:len(genres[j])-1]]=1
-          result = mydict.get((genres[j][0:len(genres[j])], arr[0][0:7]))
-          if result==None:
-              mydict[(genres[j][0:len(genres[j])], arr[0][0:7])] = 1
-          else:
-              mydict[(genres[j][0:len(genres[j])], arr[0][0:7])] = result + 1
+    arr = datalist[i].split(',')
+    genres = arr[6].split('#')
+    if genres[0]=='None\n':
+        continue
+    for j in (1,len(genres)-1):
+        result = mydict.get((genres[j][0:len(genres[j])-1], arr[year_index][0:7]))
+        genres_dict[genres[j][0:len(genres[j])-1]]=1
+        if result==None:
+            mydict[(genres[j][0:len(genres[j])-1], arr[year_index][0:7])] = 1
+        else:
+            mydict[(genres[j][0:len(genres[j])-1], arr[year_index][0:7])] = result + 1
 f_r.close()
 
 
 #データの書き込み
 f_w = open('sampledata.js','w')
-#
-f_w.write("ShopData = {\n")
-f_w.write("        genre_num: ["+len(genres_dict)+"],\n")
+#最初の方の諸々データ
+f_w.write("var ShopData=[];\n")
+f_w.write("ShopData.push({title:'Unagi Data',\n")
+f_w.write("genre_num: "+str(len(genres_dict))+",\n")
 #ジャンル
-f_w.write("        genres: [")
+f_w.write("genres: [")
 genres = []
 for key in genres_dict.keys():
     genres.append(key)
@@ -40,5 +36,66 @@ for i in range(len(genres)):
         f_w.write("\""+genres[i]+"\"],\n")
     else:
         f_w.write("\""+genres[i]+"\", ")
+f_w.write("min_date: 2011-09,\n")#手動です。ごめんなさいごめんなさいごめんなさい
+f_w.write("max_date: 2021-11,\n")
 
-#
+
+#データ
+f_w.write("dist: [\n")
+#2011年
+for month in range(9,13):
+    f_w.write("[")
+    date = "2011-"
+    if month<10:
+        date += "0"+str(month)
+    else:
+        date += str(month)
+    for i in range(len(genres)):
+        if mydict.get((genres[i],date))==None:
+            f_w.write("0")
+        else:
+            f_w.write(str(mydict[(genres[i],date)]))
+        if i==len(genres)-1:
+            f_w.write("],\n")
+        else:
+            f_w.write(", ")
+#2012-2020年
+for year in range(12,21):
+    for month in range(1,13):
+        f_w.write("[")
+        date = "20"+str(year)+"-"
+        if month<10:
+            date += "0"+str(month)
+        else:
+            date += str(month)
+        for i in range(len(genres)):
+            if mydict.get((genres[i],date))==None:
+                f_w.write("0")
+            else:
+                f_w.write(str(mydict[(genres[i],date)]))
+            if i==len(genres)-1:
+                f_w.write("],\n")
+            else:
+                f_w.write(", ")
+#2021年
+for month in range(1,12):
+    f_w.write("[")
+    date = "2021-"
+    if month<10:
+        date += "0"+str(month)
+    else:
+        date += str(month)
+    for i in range(len(genres)):
+        if mydict.get((genres[i],date))==None:
+            f_w.write("0")
+        else:
+            f_w.write(str(mydict[(genres[i],date)]))
+        if i==len(genres)-1:
+            if month==11:
+                f_w.write("]\n")
+            else:
+                f_w.write("],\n")
+        else:
+            f_w.write(", ")
+f_w.write("]});\n")
+f_w.close()
