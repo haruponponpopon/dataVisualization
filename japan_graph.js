b409d862a01e59bcae40e02531b020d249ec7393
Shiho Noda <shihonoda@ShihonoMacBook-Air.local>
2021-11-13 05:33:36 +0900

    年月変更可能&グラフに赤い線

--- japan_graph.js
+++ japan_graph.js
@@ -3,6 +3,9 @@ var true_num;
 var colors;
 var color_dict;
 var genre_list;
+var dateMax;
+var dateMin;
+var graph_width=400, graph_height=300, graph_margin=20;
 
 function pull_choice_data() {
     choice_legend = JSON.parse(sessionStorage.getItem("choice_genres"));
@@ -20,6 +23,11 @@ function push_choice_data() {
     sessionStorage.setItem("choice_genres" , JSON.stringify(choice_legend) );
     sessionStorage.setItem("true_num", true_num);
     sessionStorage.setItem("colors", JSON.stringify(colors));
+}
+
+function currentVline() {
+	var current_date = JSON.parse(sessionStorage.getItem("current_date"));
+	return (current_date - dateMin) / (dateMax - dateMin) * graph_width;
 }
 
 function MakeGraph(data, id){
@@ -43,7 +51,6 @@ function MakeGraph(data, id){
 
 	function toComma(x) {    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }
 		
-	var width=400, height=300, margin=20;
 
 	var min_date_list = data.min_date.split("-");
 	var max_date_list = data.max_date.split("-");
@@ -66,28 +73,34 @@ function MakeGraph(data, id){
 		function tH(d){ return y(minT + d*(maxT-minT)/50); }
 		
 		var svg =d3.select("#"+id).select("."+type);
+
 		
 		//x and y axis maps.
-		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, width]);
-		var y = d3.scaleLinear().domain([minT, maxT]).range([height, 0]);
+		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
+		var y = d3.scaleLinear().domain([minT, maxT]).range([graph_height, 0]);
 		
 		//draw yellow background for graph.
-		svg.append("rect").attr("x",0).attr("y",0).attr("width",width).attr("height",height).style("fill","rgb(235,235,209)");
+		svg.append("rect").attr("x",0).attr("y",0).attr("width",graph_width).attr("height",graph_height).style("fill","rgb(235,235,209)");
 		
 		// draw vertical lines of the grid.
 		svg.selectAll(".vlines").data(d3.range(data.data_num)).enter().append("line").attr("class","vlines")
 			.attr("x1",tW).attr("y1",0)
-			.attr("x2", tW).attr("y2",function(d,i){ return  (Number(min_date_list[1]) + d) % 12 == 1 && (Number(min_date_list[1]) + d) % 12 != 1? height+12: height;});
+			.attr("x2", tW).attr("y2",function(d,i){ return  (Number(min_date_list[1]) + d) % 12 == 1 && (Number(min_date_list[1]) + d) % 12 != 1? graph_height+12: graph_height;});
 		
 		//draw horizontal lines of the grid.
 		svg.selectAll(".hlines").data(d3.range(51)).enter().append("line").attr("class","hlines")
 			.attr("x1",function(d,i){ return d%10 ==0 && d!= 50? -12: 0;})
-			.attr("y1",tH).attr("x2", width).attr("y2",tH);
+			.attr("y1",tH).attr("x2", graph_width).attr("y2",tH);
 			
 		// make every 10th line in the grid darker.	
 		svg.selectAll(".hlines").filter(function(d){ return d%10==0}).style("stroke-opacity",0.7);
 		svg.selectAll(".vlines").filter(function(d){ return (Number(min_date_list[1]) + d) % 12 == 1}).style("stroke-opacity",0.7);
 		
+		svg.selectAll(".selectDate").data(d3.range(1)).enter().append("line").attr("class", "selectDate")
+		.attr("y1",0).attr("y2", graph_height)
+		.attr("x1",currentVline).attr("x2",currentVline)
+		.attr("stroke", "red").attr("stroke-width", 1);
+
 		function getHLabel(d,i){
 			if(type=="dist"){ // for distribution graph use the min and max to get the 5 label values.
 				var r= dateMin +d*(1/12); 
@@ -104,13 +117,14 @@ function MakeGraph(data, id){
 		// add horizontal axis labels
 		svg.append("g").attr("class","hlabels")
 			.selectAll("text").data(d3.range(data.data_num).filter(function(d){ return (Number(min_date_list[1]) + d) % 12 == 1;})).enter().append("text")
-			.text(getHLabel).attr("x",function(d,i){ return tW(d)+5;}).attr("y",height+14);	
+			.text(getHLabel).attr("x",function(d,i){ return tW(d)+5;}).attr("y",graph_height+14);	
 			
 		// add vertical axes labels.
 		svg.append("g").attr("class","vlabels")
 			.selectAll("text").data(d3.range(41).filter(function(d){return d%10==0; })).enter().append("text")
 			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";})
 			.text(getVLabel).attr("x",-10).attr("y",function(d){ return 5;});	
+
 		
 		var graph_line = d3.line().x(function(d) {return x(d.x); })
 			.y(function(d) { return y(d.y); });
@@ -141,8 +155,8 @@ function MakeGraph(data, id){
 		function tW(d){ return x(d); }
 		function tH(d){ return y(minT + d*(maxT-minT)/50); }
 
-		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, width]);
-		var y = d3.scaleLinear().domain([minT, maxT]).range([height, 0]);
+		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
+		var y = d3.scaleLinear().domain([minT, maxT]).range([graph_height, 0]);
 
 
 		// transition the lines, areas, and labels.
@@ -195,8 +209,8 @@ function MakeGraph(data, id){
 			}
 		}
 		
-		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, width]);
-		var y = d3.scaleLinear().domain([minCurrent, maxCurrent]).range([height, 0]);
+		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
+		var y = d3.scaleLinear().domain([minCurrent, maxCurrent]).range([graph_height, 0]);
 		
 		function tW(d){ return x(d); }
 		function tH(d){ return y(minT + d*(maxT-minT)/50); }
@@ -237,8 +251,8 @@ function MakeGraph(data, id){
 	d3.select("#"+id).append("h3").text("全国の店舗数遷移");
 	
 	// add svg and set attributes for distribution.
-	d3.select("#"+id).append("svg").attr("width",width+2*margin).attr("height",height+2*margin)
-		.append("g").attr("transform","translate("+margin+","+margin+")").attr("class","dist");
+	d3.select("#"+id).append("svg").attr("width",graph_width+2*graph_margin).attr("height",graph_height+2*graph_margin)
+		.append("g").attr("transform","translate("+graph_margin+","+graph_margin+")").attr("class","dist");
 		
 	// Draw the a graph.
 	draw("dist");			
@@ -335,4 +349,10 @@ function drawAll(data, id){
             
         d3.range(data.length).forEach(function(d,i){MakeGraph(data[i], "segment"+i );});
     });
+}
+
+function updateVline(input_data, id){
+	var svg = d3.select("#"+id).select("."+"dist");	
+	svg.selectAll(".selectDate").transition().duration(50)
+		.attr("x1",currentVline).attr("x2",currentVline);
 }
