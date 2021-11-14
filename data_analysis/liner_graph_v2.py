1d4610b4dda1e2634dbe4998e1dbc2638cf7e482
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-11-14 21:26:41 +0900

    データ集計アルゴリズムのバグの排除、複数のデータを一つにまとめた、集計したデータにミスがないかチェックし、バグの排除

--- data_analysis/liner_graph_v2.py
+++ data_analysis/liner_graph_v2.py
@@ -1,6 +1,6 @@
 #データの読み込み、分析
 #累積
-f_r = open('sampledata.csv', 'r')
+f_r = open('aggregated_data.csv', 'r')
 
 datalist = f_r.readlines()
 mydict = {}
@@ -16,7 +16,7 @@ for i in range(1,len(datalist)):
         op = 1
     if genres[0]=='None\n':
         continue
-    for j in (1,len(genres)-1):
+    for j in (range(1,len(genres))):
         result = mydict.get((genres[j][0:len(genres[j])-1], arr[year_index][0:7]))
         genres_dict[genres[j][0:len(genres[j])-1]]=1
         if result==None:
@@ -27,7 +27,7 @@ f_r.close()
 
 
 #データの書き込み
-f_w = open('sample_line.js','w')
+f_w = open('aggregated_line_v2.js','w')
 #最初の方の諸々データ
 f_w.write("var ShopData=[];\n")
 f_w.write("ShopData.push({title:'Unagi Data',\n")
@@ -37,14 +37,15 @@ f_w.write("genre: [")
 genres = []
 for key in genres_dict.keys():
     genres.append(key)
+    print(key, end=', ')
+print()
 for i in range(len(genres)):
     if i == len(genres)-1:
         f_w.write("\""+genres[i]+"\"],\n")
     else:
         f_w.write("\""+genres[i]+"\", ")
-f_w.write("min_date: \"2011-09\",\n")#手動です。ごめんなさいごめんなさいごめんなさい
+f_w.write("min_date: \"2010-08\",\n")#手動です。ごめんなさいごめんなさいごめんなさい
 f_w.write("max_date: \"2021-11\",\n")
-
 
 #データ
 f_w.write("dist: {\n")
@@ -53,9 +54,9 @@ for genre in genres:
     f_w.write("\""+genre+"\": [")
     #店の閉店開店の累積データの配列
     sum = 0
-    #2011年
-    for month in range(9,13):
-        date = "2011-"
+    #2010年
+    for month in range(8,13):
+        date = "2010-"
         if month<10:
             date += "0"+str(month)
         else:
@@ -64,7 +65,7 @@ for genre in genres:
             sum+=mydict[(genre,date)]
         f_w.write(" "+str(sum)+",")
     #2012年-2020年
-    for year in range(12,21):
+    for year in range(11,21):
         for month in range(1,13):
             date = "20"+str(year)+"-"
             if month<10:
--- kaiten_heiten_scraping/add_genres.py
+++ kaiten_heiten_scraping/add_genres.py
@@ -1,10 +1,10 @@
-f_r = open('data/yakisoba.csv', 'r')
-f_w = open('data/yakisoba_genres.csv', 'w')
+f_r = open('data/omlet.csv', 'r')
+f_w = open('data/omlet_genres.csv', 'w')
 datalist = f_r.readlines()
 f_w.write("ShopName,Date,Prefecture,longitude,latitude,Open,genres\n")
 for i in range(1,len(datalist)):
       arr = datalist[i].split(',')
       if arr[5]=="1\n":
-        f_w.write(arr[1]+","+arr[0]+","+arr[2]+","+arr[3]+","+arr[4]+",1, #焼きそば\n")
+        f_w.write(arr[1]+","+arr[0]+","+arr[2]+","+arr[3]+","+arr[4]+",1, #オムライス\n")
       else:
-        f_w.write(arr[1]+","+arr[0]+","+arr[2]+","+arr[3]+","+arr[4]+",0, #焼きそば\n")
+        f_w.write(arr[1]+","+arr[0]+","+arr[2]+","+arr[3]+","+arr[4]+",0, #オムライス\n")
