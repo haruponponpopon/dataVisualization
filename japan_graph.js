cb5fd0ae004c263304e2518bd1d83b578da23e69
Shiho Noda <shihonoda@ShihonoMacBook-Air.local>
2021-11-16 02:00:27 +0900

    全国グラフの目盛り表示の改善

--- japan_graph.js
+++ japan_graph.js
@@ -6,6 +6,7 @@ var genre_list;
 var dateMax;
 var dateMin;
 var graph_width=400, graph_height=300, graph_margin=50;
+var memori_line, memori_label, vline_upper, vline_lower;
 
 function pull_choice_data() {
     choice_legend = JSON.parse(sessionStorage.getItem("choice_genres"));
@@ -24,6 +25,60 @@ function push_choice_data() {
 function currentVline() {
 	var current_date = JSON.parse(sessionStorage.getItem("current_date"));
 	return (current_date - dateMin) / (dateMax - dateMin) * graph_width;
+}
+
+function CalcMemori(maxT, minT) {
+	var order = maxT - minT;
+	if (order <= 50) {
+		memori_line = 1;
+		if (order <= 10) {
+			memori_label = 1;
+		}
+		else {
+			memori_label = 5;
+		}
+	} 
+	else if (order <= 250) {
+		memori_line = 5;
+		if (order <= 150) {
+			memori_label = 10;
+		}
+		else {
+			memori_label = 50;
+		}
+	}
+	else if(order <= 500) {
+		memori_line = 10;
+		memori_label = 50;
+	}
+	else if(order <= 2500) {
+		memori_line = 50;
+		if (order <= 1500) {
+			memori_label = 100;
+		}
+		else {
+			memori_label = 500;
+		}
+	}
+	else {
+		memori_line = 100;
+		memori_label = 500;
+	}
+	vline_upper = maxT > 0 ? Math.floor(maxT / memori_line) + 1 : 0;
+	vline_lower = minT < 0 ? Math.floor(-minT / memori_line) + 1 : 0;
+}
+
+function calcZahyou (index) {
+	return -1 * vline_lower * memori_line + index*memori_line;
+}
+
+function reset_vlines(svg) {
+	if (svg.selectAll(".hlines").size()) {
+		svg.selectAll(".hlines").remove();
+	}
+	if (svg.selectAll(".vlabels").size()) {
+		svg.selectAll(".vlabels").remove();
+	}
 }
 
 function MakeGraph(data, id){
@@ -61,19 +116,18 @@ function MakeGraph(data, id){
 		var m = d3.min(data[type][g]);
 		minT = m < minT ? m : minT;
 	}
-
 	
 	function draw(){
-		
+		CalcMemori(maxT, minT);
 		function tW(d){ return x(d); }
-		function tH(d){ return y(minT + d*(maxT-minT)/50); }
+		function tH(d){ return y(calcZahyou(d)); }
 		
 		var svg =d3.select("#"+id).select("."+type);
 
 		
 		//x and y axis maps.
 		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
-		var y = d3.scaleLinear().domain([minT, maxT]).range([graph_height, 0]);
+		var y = d3.scaleLinear().domain([-1 * vline_lower * memori_line, vline_upper * memori_line]).range([graph_height, 0]);
 		
 		//draw yellow background for graph.
 		svg.append("rect").attr("x",0).attr("y",0).attr("width",graph_width).attr("height",graph_height).style("fill","rgb(235,235,209)");
@@ -82,14 +136,15 @@ function MakeGraph(data, id){
 		svg.selectAll(".vlines").data(d3.range(data.data_num)).enter().append("line").attr("class","vlines")
 			.attr("x1",tW).attr("y1",0)
 			.attr("x2", tW).attr("y2",function(d,i){ return  (Number(min_date_list[1]) + d) % 12 == 1 && (Number(min_date_list[1]) + d) % 12 != 1? graph_height+12: graph_height;});
-		
+
+		reset_vlines(svg);
 		//draw horizontal lines of the grid.
-		svg.selectAll(".hlines").data(d3.range(51)).enter().append("line").attr("class","hlines")
-			.attr("x1",function(d,i){ return d%10 ==0 && d!= 50? -12: 0;})
+		svg.selectAll(".hlines").data(d3.range(vline_lower+vline_upper)).enter().append("line").attr("class","hlines")
+			.attr("x1",function(d,i){ return calcZahyou(d)%memori_label == 0 && d!= vline_lower+vline_upper? -12: 0;})
 			.attr("y1",tH).attr("x2", graph_width).attr("y2",tH);
-			
+		
 		// make every 10th line in the grid darker.	
-		svg.selectAll(".hlines").filter(function(d){ return d%10==0}).style("stroke-opacity",0.7);
+		svg.selectAll(".hlines").filter(function(d){ return calcZahyou(d)%memori_label == 0}).style("stroke-opacity",0.7);
 		svg.selectAll(".vlines").filter(function(d){ return (Number(min_date_list[1]) + d) % 12 == 1}).style("stroke-opacity",0.7);
 		
 		svg.selectAll(".selectDate").data(d3.range(1)).enter().append("line").attr("class", "selectDate")
@@ -98,17 +153,13 @@ function MakeGraph(data, id){
 		.attr("stroke", "red").attr("stroke-width", 1);
 
 		function getHLabel(d,i){
-			if(type=="dist"){ // for distribution graph use the min and max to get the 5 label values.
-				var r= dateMin +d*(1/12); 
-				var year = Math.trunc(r);
-				return year;
-			}
+			var r= dateMin +d*(1/12); 
+			var year = Math.trunc(r);
+			return year;
 		}
 		
 		function getVLabel(d,i){
-			if(type=="dist"){ // for dist use the maximum for sum of frequencies and divide it into 5 pieces.
-				return Math.round(minT + (maxT - minT)*i/5);
-			}
+			return calcZahyou(d);
 		}
 		// add horizontal axis labels
 		svg.append("g").attr("class","hlabels")
@@ -117,7 +168,7 @@ function MakeGraph(data, id){
 			
 		// add vertical axes labels.
 		svg.append("g").attr("class","vlabels")
-			.selectAll("text").data(d3.range(41).filter(function(d){return d%10==0; })).enter().append("text")
+			.selectAll("text").data(d3.range(vline_lower+vline_upper).filter(function(d){return calcZahyou(d)%memori_label == 0; })).enter().append("text")
 			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";})
 			.text(getVLabel).attr("x",-10).attr("y",function(d){ return 5;});	
 
@@ -150,103 +201,6 @@ function MakeGraph(data, id){
 			.attr("fill", "none");
 
 			
-	}
-	
-	function transitionDefault(){
-		
-		function tW(d){ return x(d); }
-		function tH(d){ return y(minT + d*(maxT-minT)/50); }
-
-		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
-		var y = d3.scaleLinear().domain([minT, maxT]).range([graph_height, 0]);
-
-
-		// transition the lines, areas, and labels.
-		var svg = d3.select("#"+id).select("."+type);	
-
-		var graph_line = d3.line().x(function(d) { return x(d.x); })
-			.y(function(d) { return y(d.y); });
-
-		var dataset = getPoints(data[type]);
-
-		svg.selectAll("path")
-			.data(dataset)
-			.transition().duration(500)
-			.attr("d", graph_line)
-			.attr("stroke-width",function(d, i) {
-				if (choice_legend[ganre_list[i]]) {
-					return 2;
-				}
-				return 0;
-			})
-			.attr("stroke", function(d, i) {
-				if (choice_legend[ganre_list[i]]) {
-					return colors[color_dict[ganre_list[i]]].color;
-				}
-				return "#000000";
-			})
-			.attr("fill", "none");
-
-		svg.selectAll(".vlines").transition().duration(500).attr("x1",tW).attr("x2", tW);			
-		svg.selectAll(".hlines").transition().duration(500).attr("y1",tH).attr("y2",tH);			
-		svg.selectAll(".vlabels").selectAll("text").transition().duration(500)
-			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";});	
-			
-	}
-
-
-    function transitionClick(){
-        if (true_num == 0) {
-            transitionDefault();
-            return;
-        }
-		var maxCurrent = 0;
-		var minCurrent = 0;
-		for (var g in data[type]) {
-			if (choice_legend[g]) {
-				var M = d3.max(data[type][g]);
-				maxCurrent = M > maxCurrent ? M : maxCurrent;
-				var m = d3.min(data[type][g]);
-				minCurrent = m < minCurrent ? m : minCurrent;
-			}
-		}
-		
-		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
-		var y = d3.scaleLinear().domain([minCurrent, maxCurrent]).range([graph_height, 0]);
-		
-		function tW(d){ return x(d); }
-		function tH(d){ return y(minT + d*(maxT-minT)/50); }
-
-		var svg = d3.select("#"+id).select("."+type);
-		//transition all the lines, labels, and areas.
-		var graph_line = d3.line().x(function(d) {return x(d.x); })
-			.y(function(d) { return y(d.y); });
-
-		var dataset = getPoints(data[type]);
-
-		svg.selectAll("path")
-			.data(dataset)
-			.transition().duration(500)
-			.attr("d", graph_line).attr("fill", "none")
-			.attr("stroke-width",function(d, i) {
-				if (choice_legend[ganre_list[i]]) {
-					return 2;
-				}
-				return 0;
-			})
-			.attr("stroke", function(d, i) {
-				if (choice_legend[ganre_list[i]]) {
-					return colors[color_dict[ganre_list[i]]].color;
-				}
-				return "#000000";
-			});
-
-			
-		svg.selectAll(".vlines").transition().duration(500).attr("x1",tW).attr("x2", tW);			
-		svg.selectAll(".hlines").transition().duration(500).attr("y1",tH).attr("y2",tH);						
-		svg.selectAll(".vlabels").selectAll("text").transition().duration(500)
-			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";});	
-		
 	}
 
 	// add title.
@@ -293,16 +247,33 @@ function updateGraph(data, id) {
 
 
 	function transitionDefault(){
-		
+		CalcMemori(maxT, minT);
 		function tW(d){ return x(d); }
-		function tH(d){ return y(minT + d*(maxT-minT)/50); }
+		function tH(d){ return y(calcZahyou(d)); }
 
 		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
-		var y = d3.scaleLinear().domain([minT, maxT]).range([graph_height, 0]);
+		var y = d3.scaleLinear().domain([-1 * vline_lower * memori_line, vline_upper * memori_line]).range([graph_height, 0]);
 
 
 		// transition the lines, areas, and labels.
-		var svg = d3.select("#"+id).select("."+type);	
+		var svg = d3.select("#"+id).select("."+type);
+		reset_vlines(svg);
+		//draw horizontal lines of the grid.
+		svg.selectAll(".hlines").data(d3.range(vline_lower+vline_upper)).enter().append("line").attr("class","hlines")
+			.attr("x1",function(d,i){ return calcZahyou(d)%memori_label == 0 && d!= vline_lower+vline_upper? -12: 0;})
+			.attr("y1",tH).attr("x2", graph_width).attr("y2",tH);
+		
+		// make every 10th line in the grid darker.	
+		svg.selectAll(".hlines").filter(function(d){ return calcZahyou(d)%memori_label == 0}).style("stroke-opacity",0.7);	
+
+		function getVLabel(d,i){
+			return calcZahyou(d);
+		}
+		// add vertical axes labels.
+		svg.append("g").attr("class","vlabels")
+			.selectAll("text").data(d3.range(vline_lower+vline_upper).filter(function(d){return calcZahyou(d)%memori_label == 0; })).enter().append("text")
+			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";})
+			.text(getVLabel).attr("x",-10).attr("y",function(d){ return 5;});	
 
 		var graph_line = d3.line().x(function(d) { return x(d.x); })
 			.y(function(d) { return y(d.y); });
@@ -328,10 +299,6 @@ function updateGraph(data, id) {
 			.attr("fill", "none");
 
 		svg.selectAll(".vlines").transition().duration(500).attr("x1",tW).attr("x2", tW);			
-		svg.selectAll(".hlines").transition().duration(500).attr("y1",tH).attr("y2",tH);			
-		svg.selectAll(".vlabels").selectAll("text").transition().duration(500)
-			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";});	
-			
 	}
 
 
@@ -350,14 +317,35 @@ function updateGraph(data, id) {
 				minCurrent = m < minCurrent ? m : minCurrent;
 			}
 		}
+		CalcMemori(maxCurrent, minCurrent);
 		
 		var x = d3.scaleLinear().domain([0, data.data_num - 1]).range([0, graph_width]);
-		var y = d3.scaleLinear().domain([minCurrent, maxCurrent]).range([graph_height, 0]);
+		var y = d3.scaleLinear().domain([-1 * vline_lower * memori_line, vline_upper * memori_line]).range([graph_height, 0]);
 		
 		function tW(d){ return x(d); }
-		function tH(d){ return y(minT + d*(maxT-minT)/50); }
+		function tH(d){ return y(calcZahyou(d)); }
 
 		var svg = d3.select("#"+id).select("."+type);
+
+		reset_vlines(svg);
+		//draw horizontal lines of the grid.
+		svg.selectAll(".hlines").data(d3.range(vline_lower+vline_upper)).enter().append("line").attr("class","hlines")
+			.attr("x1",function(d,i){ return calcZahyou(d)%memori_label == 0 && d!= vline_lower+vline_upper? -12: 0;})
+			.attr("y1",tH).attr("x2", graph_width).attr("y2",tH);
+		
+		// make every 10th line in the grid darker.	
+		svg.selectAll(".hlines").filter(function(d){ return calcZahyou(d)%memori_label == 0}).style("stroke-opacity",0.7);	
+
+		function getVLabel(d,i){
+			return calcZahyou(d);
+		}
+		// add vertical axes labels.
+		svg.append("g").attr("class","vlabels")
+			.selectAll("text").data(d3.range(vline_lower+vline_upper).filter(function(d){return calcZahyou(d)%memori_label == 0; })).enter().append("text")
+			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";})
+			.text(getVLabel).attr("x",-10).attr("y",function(d){ return 5;});	
+
+
 		//transition all the lines, labels, and areas.
 		var graph_line = d3.line().x(function(d) {return x(d.x); })
 			.y(function(d) { return y(d.y); });
@@ -383,9 +371,7 @@ function updateGraph(data, id) {
 
 			
 		svg.selectAll(".vlines").transition().duration(500).attr("x1",tW).attr("x2", tW);			
-		svg.selectAll(".hlines").transition().duration(500).attr("y1",tH).attr("y2",tH);						
-		svg.selectAll(".vlabels").selectAll("text").transition().duration(500)
-			.attr("transform",function(d,i){ return "translate(-10,"+(tH(d)-14)+")rotate(-90)";});	
+		
 	}
 	transitionClick();
 
